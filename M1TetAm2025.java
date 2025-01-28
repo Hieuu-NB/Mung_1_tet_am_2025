48616887e48e0372a4729d7c07ca7192a6be5c96
hieudm.tt <hieudm.tt@mcredit.com.vn>
2025-01-29 02:40:18 +0700

    khai bút năm mới
    khai bút năm mới

---
+++ M1TetAm2025.java
@@ -0,0 +1,13 @@
+public class M1TetAm2025 {
+    public static void main(String[] args) {
+        System.out.println("\n\n Mục tiêu đặt ra cho 2025 : \n\n");
+
+        System.out.println("- Tháng 2 thi xong bằng lái A2 \n");
+        System.out.println("- Tháng 4 thi xong chứng chỉ OCA \n");
+        System.out.println("- Tháng 5 bảo vệ đồ án tốt nghiệp tốt \n");
+        System.out.println("- Tháng 6 có bằng Đại Học\n");
+        System.out.println("- Tháng 6 lên ho ở MCredit \n");
+        System.out.println("- Tiếng anh tốt\n");
+        System.out.println("- Tập Calisthenic làm được front level");
+    }
+}
